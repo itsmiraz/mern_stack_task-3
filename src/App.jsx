8effb8625ae1e25dcd553c69a9d98ca91a20a33b
itsmiraz <mirajulislamofficial044@gmail.com>
2023-02-13 22:16:34 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useQuery } from "@tanstack/react-query"
+import { useState } from "react"
+import { HashLoader } from "react-spinners"
+import UserCard from "./Components/UserCard/UserCard"
 
 
 function App() {
-
+  const [selectedUser, setSelectedUser] = useState('')
 
 
 
@@ -16,11 +19,55 @@ function App() {
   })
 
 
-  console.log(users)
+  console.log(typeof (selectedUser))
+
 
   return (
-    <div>
+    <div className="max-w-[1200px] mx-auto grid px-20 grid-cols-1  md:grid-cols-2 pt-20 gap-20">
+      <div>
+        <h1 className="text-xl text-center rounded-t bg-[#C5DFFF] w-full py-4">USERS LIST</h1>
 
+        {
+          isLoading ?
+              <div className="h-[200px] flex justify-center items-center">
+                <HashLoader color="#367fd6" />
+              </div>
+            :
+            <>
+              <div>
+                {
+                  users?.slice(0, 10).map(user => <UserCard
+                    key={user.id}
+                    user={user}
+                    setSelectedUser={setSelectedUser}
+                  />)
+                }
+              </div>
+            </>
+        }
+      </div>
+      <div>
+        <h1 className="text-xl text-center  rounded-t bg-[#C5DFFF] w-full py-4">USER DETAILS</h1>
+
+        {
+          selectedUser ?
+            <div className="flex flex-col gap-y-2 font-semibold  justify-center">
+              <img className="w-40 mx-auto" src='https://cdn.vectorstock.com/i/preview-1x/53/42/user-member-avatar-face-profile-icon-vector-22965342.jpg' alt="" />
+              <p className="text-center font-semibold text-2xl">@{selectedUser.profile.username}</p>
+              <p className="p-2 bg-[#DBDBDB] font-semibold">{ selectedUser.Bio}</p>
+              <p>Full Name</p>
+              <p className="p-2 bg-[#DBDBDB]">{selectedUser.profile.firstName} { selectedUser.profile.lastName}</p>
+              <p>Job Title</p>
+              <p className="p-2 bg-[#DBDBDB]">{selectedUser.jobTitle}</p>
+              <p>Email</p>
+              <p className="p-2 bg-[#DBDBDB]">{ selectedUser.profile.email}</p>
+            </div>
+            :
+            <div className="h-[400px] flex justify-center text-center items-center">
+              <p className="font-semibold text-xl text-gray-700">Please Select a User</p>
+            </div>
+        }
+      </div>
     </div>
   )
 }
--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const UserCard = () => {
+const UserCard = ({ user,setSelectedUser }) => {
+
+
+    const {
+        Bio,
+        avatar,
+        createdAt,
+        id,
+        jobTitle,
+        profile
+    } = user
+   console.log(user)
+   
     return (
-        <div>
-            
+        <div onClick={() => setSelectedUser(user)} className='bg-[#ECECEC] hover:bg-[#D5D5D5] flex gap-2 rounded items-center px-2 shadow-md my-4 py-2 font-semibold '>
+            <img className='w-12 rounded-full' src='https://cdn.vectorstock.com/i/preview-1x/53/42/user-member-avatar-face-profile-icon-vector-22965342.jpg' alt="" />
+            <p>{profile.firstName} { profile.lastName}</p>
         </div>
     );
 };
